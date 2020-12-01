db9a9c99c218664006e120353d1c238b61336a6a
Julian Borghoff <julian_tobias.borghoff@edu.FHDW.de>
2020-12-01 18:55:22 +0100

    …the actual code

---
+++ AdventureOfCode2020/FirstOfDecember/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace FirstOfDecember
+{
+    class Program
+    {
+        static void Main()
+        {
+            var input = File.ReadAllLines("input.txt").Select(x => int.Parse(x));
+
+            //Yeah it's a little bit ugly, but I've promised someone to do this in one line ;)
+            var solution = input.Select(x => input.FirstOrDefault(y => y + x == 2020) * x).First(x => x != 0);
+            
+            Console.WriteLine($"The result for the first day of AdventOfCode is: {solution}");
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
