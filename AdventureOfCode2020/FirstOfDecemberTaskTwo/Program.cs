dceda8bc2eff148154b847a4fa8a21eae9c2525d
Julian Borghoff <julian_tobias.borghoff@edu.FHDW.de>
2020-12-01 19:13:09 +0100

    …solution for Day #1 Task #2

---
+++ AdventureOfCode2020/FirstOfDecemberTaskTwo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace FirstOfDecemberTaskTwo
+{
+    class Program
+    {
+        static void Main()
+        {
+            var input = File.ReadAllLines("input.txt").Select(x => int.Parse(x));
+
+            //Yeah it's a little bit ugly, but I've promised someone to do this in one line - even If it gets even more messy ;)
+            var solution = 
+                input.Select(x =>
+                    input.Select(y => 
+                        input.FirstOrDefault(z => x + y + z == 2020) * y)
+                    .FirstOrDefault(x => x != 0) * x
+                ).FirstOrDefault(x => x != 0);
+
+            
+            Console.WriteLine($"The result for the first day of AdventOfCode is: {solution}");
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
