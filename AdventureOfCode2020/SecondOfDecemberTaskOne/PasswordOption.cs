01d1b59a15302ed94d755f039b5153828af231b1
Julian Borghoff <julian_tobias.borghoff@edu.FHDW.de>
2020-12-02 19:40:21 +0100

    Finished Solution for 2.12.2020 Task #1

---
+++ AdventureOfCode2020/SecondOfDecemberTaskOne/PasswordOption.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace SecondOfDecemberTaskOne
+{
+    public class PasswordOption
+    {
+        //static regex objects to prevent compiling the regex patterns multiple times
+       // private static readonly Regex OccurrenceRegex = new Regex(@"([0-9]+)(?=-)");
+        private static readonly Regex OccurrenceRegex = new Regex(@"([0-9]+)");
+        private static readonly Regex PassCharRegex = new Regex(@"([a-z]+)(?=:)");
+        private static readonly Regex PasswordRegex = new Regex(@"([a-z]+)(?!\\w)(?!:)");
+        
+        public int MinOccurrence { get; private set; }
+        public int MaxOccurrence { get; private set; }
+        public char PassChar { get; private set; }
+        public String Password { get; private set; }
+        public int PassCharCount { get; private set; }
+        public bool PasswordValid { get; private set; }
+
+        public PasswordOption(String PassLine)
+        {
+            SetMinMaxOccurrence(PassLine);
+            SetPassChar(PassLine);
+            SetPassword(PassLine);
+
+            if ((MinOccurrence + "-" + MaxOccurrence + " " + PassChar + ": " + Password) != PassLine)
+            {
+                throw new Exception("Parsing error");
+            }
+            
+            PassCharCount = Password.Count(x => x.Equals(PassChar));
+            PasswordValid = (MinOccurrence <= PassCharCount && PassCharCount <= MaxOccurrence);
+        }
+
+        private void SetMinMaxOccurrence(String input)
+        {
+            var minOutput = OccurrenceRegex.Matches(input)[0].Value;
+            var maxOutput = OccurrenceRegex.Matches(input)[1].Value;
+            MinOccurrence = Int32.Parse(minOutput);
+            MaxOccurrence = Int32.Parse(maxOutput);
+        }
+
+        private void SetPassChar(String input)
+        {
+            var output = PassCharRegex.Match(input).Value;
+            PassChar = char.Parse(output);
+        }
+
+        private void SetPassword(String input)
+        {
+            var output = PasswordRegex.Match(input).Value;
+            Password = output;
+        }
+    }
+}
--- AdventureOfCode2020/SecondOfDecemberTaskOne/Program.cs
+++ AdventureOfCode2020/SecondOfDecemberTaskOne/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace SecondOfDecemberTaskOne
 {
@@ -6,7 +8,12 @@ namespace SecondOfDecemberTaskOne
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var input = File.ReadAllLines("input.txt");
+
+            var passwordOptions = input.Select(x => new PasswordOption(x));
+            var validOptions = passwordOptions.Count(x => x.PasswordValid);
+            
+            Console.WriteLine($"Valid option count for the advent of code challenge on 2nd December of 2020 Task #1 is: {validOptions}");
         }
     }
 }
