bc48636f657cfe9e2d6dfeb6e9e0184269241325
Julian Borghoff <julian_tobias.borghoff@edu.FHDW.de>
2020-12-02 19:52:39 +0100

    Finished Solution for 2.12.2020 Task #2

---
+++ AdventureOfCode2020/SecondOfDecemberTaskTwo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace SecondOfDecemberTaskTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("input.txt");
+
+            var passwordOptions = input.Select(x => new PasswordOption(x));
+            var validOptions = passwordOptions.Count(x => x.PasswordValid);
+            
+            Console.WriteLine($"Valid option count for the advent of code challenge on 2nd December of 2020 Task #1 is: {validOptions}");
+        }
+    }
+}
