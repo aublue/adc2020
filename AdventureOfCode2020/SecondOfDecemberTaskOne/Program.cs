b0862ca97353933f76913d97236612dde4b44d31
Julian Borghoff <julian_tobias.borghoff@edu.FHDW.de>
2020-12-02 12:55:10 +0100

    Initial commit of empty project

---
+++ AdventureOfCode2020/SecondOfDecemberTaskOne/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SecondOfDecemberTaskOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
