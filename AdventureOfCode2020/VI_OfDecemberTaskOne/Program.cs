11a0bc85b0bbf33069f1dedf741ad9baacacf3f1
Julian Borghoff <julian_tobias.borghoff@edu.FHDW.de>
2020-12-05 19:37:52 +0100

    Created empty Projects for 05.12.2020 / upgraded to .net 5

---
+++ AdventureOfCode2020/VI_OfDecemberTaskOne/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VI_OfDecemberTaskOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ AdventureOfCode2020/VI_OfDecemberTaskTwo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VI_OfDecemberTaskTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
