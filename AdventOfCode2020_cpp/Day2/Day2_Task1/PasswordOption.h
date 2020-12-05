008de4b56cf81ea494645a15fdf60cc5de2f5c38
Julian Borghoff <julian_tobias.borghoff@edu.FHDW.de>
2020-12-05 23:49:28 +0100

    My c++ version for task 1 of day 2 - as it seems the performance is catastrophic compared to my c# implementation

---
+++ AdventOfCode2020_cpp/Day2/Day2_Task1/PasswordOption.h
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <regex>
+//
+// Created by aublue on 05.12.20.
+//
+
+#ifndef DAY2_TASK1_PASSWORDOPTION_H
+#define DAY2_TASK1_PASSWORDOPTION_H
+
+using namespace std;
+
+class PasswordOption {
+
+private:
+    //Specifiying Regex for parsing the input.
+    const regex OccurrenceRegex = regex("([0-9]+)");
+    const regex PassCharRegex = regex("([a-z]+)(?=:)");
+    const regex PasswordRegex = regex("([a-z]+)(?!\\w)(?!:)");
+
+    //Private Variables
+
+    //Private Methods
+    void _SetMinMaxOccurrence(string input);
+    void _SetPassChar(string input);
+    void _SetPassword(string input);
+    std::vector<string> _GetAllMatches(regex reg, string inputstr);
+
+public:
+    //Public Variables
+    int MinOccurrence = 0;
+    int MaxOccurrence = 0;
+    char PassChar = ' ';
+    string Password = "";
+    int PassCharCount = 0;
+    bool PasswordValid = false;
+
+    //Constructors
+    PasswordOption(string s);
+
+    //Public Methods
+};
+
+
+#endif //DAY2_TASK1_PASSWORDOPTION_H
