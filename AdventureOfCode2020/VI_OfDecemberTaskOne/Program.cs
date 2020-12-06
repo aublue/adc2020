a7cdea2e01afeeea74c211ab97ce110e2e8fb750
Julian Borghoff <julian_tobias.borghoff@edu.FHDW.de>
2020-12-06 13:09:19 +0100

    Solution for Day 6 Task 2

--- AdventureOfCode2020/VI_OfDecemberTaskOne/Program.cs
+++ AdventureOfCode2020/VI_OfDecemberTaskOne/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace VI_OfDecemberTaskOne
 {
@@ -6,7 +9,36 @@ namespace VI_OfDecemberTaskOne
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var input = File.ReadAllLines("input.txt");
+            var inputLists = ParseInput(input);
+
+            var totalCount = inputLists.Sum(x => x.Replace(" ", "").Distinct().Count());
+            
+            Console.WriteLine($"Total answer count: {totalCount}");
+        }
+        
+        /// <summary>
+        /// This Method groups all strings separated by a blank line. 
+        /// </summary>
+        /// <param name="inputStr"></param>
+        /// <returns></returns>
+        private static IEnumerable<string> ParseInput(String[] inputStr)
+        {
+            var passportStrings = new List<String>();
+            
+            var currStrGroup = "";
+            for (var i = 0; i < inputStr.Length; i++)
+            {
+                var line = inputStr[i];
+                currStrGroup += " " + line;
+                
+                if (String.IsNullOrWhiteSpace(line) || i == inputStr.Length - 1)
+                {
+                    var strCpy = currStrGroup;
+                    currStrGroup = "";
+                    yield return strCpy;
+                }
+            }
         }
     }
 }
--- AdventureOfCode2020/VI_OfDecemberTaskTwo/Program.cs
+++ AdventureOfCode2020/VI_OfDecemberTaskTwo/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Transactions;
 
 namespace VI_OfDecemberTaskTwo
 {
@@ -6,7 +9,33 @@ namespace VI_OfDecemberTaskTwo
     {
         static void Main(string[] args)
         {
+            var input = File.ReadAllLines("input.txt");
+            var inputLines = ParseInput(input);
             Console.WriteLine("Hello World!");
+        }
+        
+        /// <summary>
+        /// This Method groups all strings separated by a blank line. 
+        /// </summary>
+        /// <param name="inputStr"></param>
+        /// <returns></returns>
+        private static IEnumerable<string> ParseInput(String[] inputStr)
+        {
+            var passportStrings = new List<String>();
+            
+            var currStrGroup = "";
+            for (var i = 0; i < inputStr.Length; i++)
+            {
+                var line = inputStr[i];
+                currStrGroup += " " + line;
+                
+                if (String.IsNullOrWhiteSpace(line) || i == inputStr.Length - 1)
+                {
+                    var strCpy = currStrGroup;
+                    currStrGroup = "";
+                    yield return strCpy;
+                }
+            }
         }
     }
 }
